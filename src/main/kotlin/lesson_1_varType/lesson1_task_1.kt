0f8db8911771e89864806d46c0773f5dc3cd1be3
d3v4k4nt <devakantjaendra@yandex.ru>
2023-04-23 22:40:00 +0600

    [Пересобрал проект]

---
+++ src/main/kotlin/lesson_1_varType/lesson1_task_1.kt
@@ -0,0 +1,11 @@
+package lesson_1_varType
+
+//e-shop logic
+
+fun main (args: Array<String>) {
+    var totalNumbersOfOrders: Int = 75
+    var textWithThanks: String = "Спасибо за покупку в нашем магазине. Ждём вас снова!"
+
+    println("Общее количество заказов на сегодня: $totalNumbersOfOrders")
+    println(textWithThanks)
+}
