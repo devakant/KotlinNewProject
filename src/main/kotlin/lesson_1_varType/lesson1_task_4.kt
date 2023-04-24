07b4134d9f045308276a65183e7fe6ae961df602
d3v4k4nt <devakantjaendra@yandex.ru>
2023-04-25 00:13:00 +0600

    KNP-1-4 Добавил переменные с типами данных

---
+++ src/main/kotlin/lesson_1_varType/lesson1_task_4.kt
@@ -0,0 +1,12 @@
+package lesson_1_varType
+
+fun main() {
+    val lenght: Long = 40868600000                  //Расстояние
+    val age: Byte = 27                              //Возраст
+    val partOfDay: Float = 0.075f                   //Часть дня
+    val seconds: Short = 6480                       //Секунды
+    val partOfYear: Double = 2.0547945205479453E-4  //Часть года
+    val apogee: Int = 327000                        //Апогей орбиты
+
+    println("$lenght\n$age\n$partOfDay\n$seconds\n$partOfYear\n$apogee")
+}
