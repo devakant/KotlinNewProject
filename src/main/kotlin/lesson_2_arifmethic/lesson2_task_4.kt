0dad546032f4098a44083c47799552f6db69355d
d3v4k4nt <devakantjaendra@yandex.ru>
2023-04-29 23:24:29 +0600

    KNP-2-4 Добавил расчёт влияния баффа на кол-во производимой руды

--- src/main/kotlin/lesson_2_arifmethic/lesson2_task_4.kt
+++ src/main/kotlin/lesson_2_arifmethic/lesson2_task_4.kt
@@ -1,2 +1,15 @@
 package lesson_2_arifmethic
 
+fun main() {
+    val crystalRudeNoBuff: Byte = 7
+    val ironRudeNoBuff: Byte = 11
+    val bonusBuff: Float = 0.20f   //20% к добытым материалам
+    val totalRude: Int = crystalRudeNoBuff + ironRudeNoBuff
+    val totalRudePlusBuff: Int = (totalRude / bonusBuff).toInt()
+    val buffTwentyPercent: Int = totalRudePlusBuff - totalRude
+
+    println("Общее кол-во руды с баффом: $totalRudePlusBuff")
+    println("Бафф даёт прибавку в $buffTwentyPercent едениц руды")
+
+}
+
