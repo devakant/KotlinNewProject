b71238e8ed9744967a02fbd362fec0c0aabfdcf6
d3v4k4nt <devakantjaendra@yandex.ru>
2023-04-29 22:31:19 +0600

    KNP-2-3 Добавил рассчёт времени в пути

--- src/main/kotlin/lesson_2_arifmethic/lesson2_task_3.kt
+++ src/main/kotlin/lesson_2_arifmethic/lesson2_task_3.kt
@@ -1,2 +1,12 @@
 package lesson_2_arifmethic
 
+fun main() {
+    val startTrainTimeHour: Byte = 9
+    val startTrainTimeMinute: Byte = 39
+    val travelTimeMinute: Short = 457
+    val travelTime: Int = travelTimeMinute / 60
+    val travelTimeHour: Int = travelTimeMinute / (60 / 7)
+
+    println("Время отбытия поезда: $startTrainTimeHour:$startTrainTimeMinute")
+    println("Время в пути: $travelTime:$travelTimeHour")
+}
