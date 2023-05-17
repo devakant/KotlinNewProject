c9d8db1c3ecbf989c696418cd8a39d3a958e1df2
d3v4k4nt <devakantjaendra@yandex.ru>
2023-05-18 01:31:59 +0600

    KNP-3-1 Добавил приветствие для для и вечера

--- src/main/kotlin/lesson_3_Interpolicy/lesson3_task_1.kt
+++ src/main/kotlin/lesson_3_Interpolicy/lesson3_task_1.kt
@@ -1,2 +1,12 @@
 package lesson_3_Interpolicy
 
+fun main() {
+    var greetings: String = "Добрый день,"
+    val userName: String = "Иван"
+
+    println("$greetings $userName!")
+
+    greetings = "Добрый вечер,"
+
+    println("$greetings $userName!")
+}
