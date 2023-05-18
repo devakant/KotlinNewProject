9c4a771800dda30212aea8ec8702c67bac8dd815
d3v4k4nt <devakantjaendra@yandex.ru>
2023-05-18 11:24:34 +0600

    KNP-3-1 Исправил вывод знаков препинаний

--- src/main/kotlin/lesson_3_Interpolicy/lesson3_task_1.kt
+++ src/main/kotlin/lesson_3_Interpolicy/lesson3_task_1.kt
@@ -1,12 +1,12 @@
 package lesson_3_Interpolicy
 
 fun main() {
-    var greetings: String = "Добрый день,"
+    var greetings: String = "Добрый день"
     val userName: String = "Иван"
 
-    println("$greetings $userName!")
+    println("$greetings, $userName!")
 
-    greetings = "Добрый вечер,"
+    greetings = "Добрый вечер"
 
-    println("$greetings $userName!")
+    println("$greetings, $userName!")
 }
