2b11713f5db44cd23b5ebfb7395f0b09bce5e492
d3v4k4nt <devakantjaendra@yandex.ru>
2023-05-20 00:25:23 +0600

    KNP-2-4 Исправил вывод процента

--- src/main/kotlin/lesson_2_arifmethic/lesson2_task_4.kt
+++ src/main/kotlin/lesson_2_arifmethic/lesson2_task_4.kt
@@ -3,13 +3,15 @@ package lesson_2_arifmethic
 fun main() {
     val crystalRudeNoBuff: Byte = 7
     val ironRudeNoBuff: Byte = 11
-    val bonusBuff: Float = 0.02f   //20% к добытым материалам
     val totalRude: Int = crystalRudeNoBuff + ironRudeNoBuff
-    val totalRudePlusBuff: Int = (totalRude / bonusBuff).toInt()
-    val buffTwentyPercent: Int = totalRudePlusBuff - totalRude
 
-    println("Общее кол-во руды с баффом: $totalRudePlusBuff")
-    println("Бафф даёт прибавку в $buffTwentyPercent едениц руды")
+    val bonusBuff: Float = 20.0f   //20% к добытым материалам
+    val crystalRudePlusBuff: Int = (totalRude * bonusBuff / 100.0f).toInt()
+    val ironRudePlusBuff: Int = (totalRude * bonusBuff / 100.0f).toInt()
+    val totalRudeInBuff: Int = totalRude + ironRudePlusBuff + crystalRudePlusBuff
+
+    println("Общее кол-во руды без баффа: $totalRude")
+    println("Общее кол-во руды с баффом в 20%: $totalRudeInBuff")
+    println("Бафф даёт прибавку $crystalRudePlusBuff еденицы для кристальной руды \nИ $ironRudePlusBuff еденицы для железной руды ")
 
 }
-
